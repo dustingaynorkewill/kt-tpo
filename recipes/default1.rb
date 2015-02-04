81972f8eedb775c0e0aa6c8d187844ee132d40f9
dustingaynorkewill <dustin.gaynor@kewill.com>
2015-02-04 16:01:16 -0600

    added remote file upload

---
+++ recipes/default1.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: kt-tpo
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+src_rpm = "transport33_13.win.exe"
+src_filename = "transport33_13.win.exe"
+src_filepath = "#{Chef::Config['file_cache_path']}/#{src_filename}"
+
+remote_file src_filepath do
+  source "http://ktwiki.kewill.com/files/#{src_filename}"
+  action :create_if_missing
+end
